7ab2bf9ad5c3290cb5544536889549cb08bcb1a3
Nick Stone <35820605+Stonen2@users.noreply.github.com>
2018-10-12 14:19:26 -0400

    Add files via upload
    Base Builds

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+       //
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Welcome()
+        {
+            ViewData["Message"] =  "This is the Welcome action method...";
+            return View(); 
+
+        }
+    }
+}
