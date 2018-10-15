42f93a79c18bc507d4c2b6231c765cda460cd606
Nick Stone <35820605+Stonen2@users.noreply.github.com>
2018-10-15 15:03:49 -0400

    Add files via upload

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Test3.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+        public string testing()
+        {
+
+
+
+            return "I hate when i understand things. i really do " +
+                "haha\n" +
+                "this is kidna nice\n" +
+                "is this java?" +
+                "or C#";
+        }
+
+        public string test2(string h)
+        {
+            return h + "  This is how you pass parameters!";
+        }
+    }
+}
